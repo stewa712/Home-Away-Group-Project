7ae9ab209d61cbb0769b95293cb6478179269dc6
aaliade <102116047+aaliade@users.noreply.github.com>
2022-11-13 15:13:34 -0600

    Add files via upload

--- Administrator.java
+++ Administrator.java
@@ -1,7 +1,4 @@
-//Created By Zachary Stewart
-//ID: 2101823
-
-package HomeAway;
+package records;
 
 //Importing scanner package to receive input from the user
 import java.util.Scanner;
@@ -10,16 +7,22 @@ import java.util.Scanner;
 import java.io.*;
 import java.util.*;
 
+import java.io.FileNotFoundException;
+import java.lang.SecurityException;
+import java.lang.IllegalStateException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
+import java.util.NoSuchElementException;
 
 
 //Creating Administrator Parent Class
 
 public class Administrator {	
-	
-	
-	
+		
 	//Attributes of Administrator Class
-	
 	protected int id;
 	protected Name name;
 	protected String username;
@@ -31,8 +34,7 @@ public class Administrator {
 		id = 0000;
 		name = new Name();
 		username = "";
-		password = "";
-		
+		password = "";	
 	}
 	
 	// Primary Constructor 
@@ -41,8 +43,7 @@ public class Administrator {
 		this.id = id;
 		this.name = name;
 		this.username = username;
-		this.password = password;
-		
+		this.password = password;	
 	}
 	
 	// Copy Constructor
@@ -55,7 +56,7 @@ public class Administrator {
 		
 	}
 
-	//Generating the Accesors and Mutators
+	//Generating the Accessors and Mutators
 	public int getID() {
 		return id;
 	}
@@ -90,19 +91,20 @@ public class Administrator {
 
 	//Method to enter data for new Staff Member and write to file
 	public void newStaff() {
-
-		
-		
 		Scanner s= new Scanner(System.in);
 		Scanner s1= new Scanner(System.in);
 		Scanner INPUT = new Scanner (System.in);
-			
-		String CHOICE= " ";   
+		Scanner input = new Scanner (System.in);
+		Formatter output = null;
+
+		
+		
+		String CHOICE = " ";   
 	    System.out.print("Staff Record File Access");
 	    System.out.print("\n\n");	
 		    
-		  while(CHOICE.charAt(0) != 'q')
-		     {
+		while(CHOICE.charAt(0) != 'q')
+		{
 		    	 System.out.print("\n\n");	
 		    	 System.out.print("\n----------- Menu -------------");	
 		    	 System.out.print("\n|  (C)reate A New Staff File |");
@@ -117,70 +119,108 @@ public class Administrator {
 		    	 
 		    	 switch(CHOICE.charAt(0))
 		    	 {
-		    	 case 'c': 
-		    	
-		    		 //Creating the file 
-		    			File myFile = new File ("staff.txt");
-		    			
-		    			try {
-		    				if(myFile.createNewFile())
+		    	 case 'c':
+		    		 try
+		    		 {
+		    			 output = new Formatter ("staff.txt");
+		    	     }
+		    				catch (SecurityException securityException)
 		    				{
-		    					System.out.println("File Ceated Scuccessfully");
+		    					System.err.println("Write permission denied. Terminating.");
+		    					System.exit(1); // terminate the program
 		    				}
-		    				else {
-		    					System.out.println("File Creation Error");
-		    				} 
-		    			} catch (IOException e) {
-		    				System.out.println("File Error");
-		    			}
-		    		 
+		    				catch (FileNotFoundException fileNotFoundException)
+		    				{
+		    					System.err.println("Error opening file. Terminating.");
+		    					System.exit(1); // terminate the program
+		    				}
 		    		 break;
 		    		    		 
 		    	 case 'a':
-		    		 	System.out.println("Enter Staff ID: ");
-						int code = s.nextInt();
-												
-						System.out.println("Enter First Name: ");
-						String fname = s1.nextLine();
-						
-						System.out.println("Enter Middle Name: ");
-						String mname = s1.nextLine();
-						
-						System.out.println("Enter Last Name: ");
-						String lname = s1.nextLine();
-										
-						System.out.println("Enter Username: ");
-						String usrname = s1.nextLine();									
-													
-						System.out.println("Enter Password: ");
-						String password = s1.nextLine();
-						
-						
-		 			try {
-		 			FileWriter output = new FileWriter ("staff.txt",true);
-		 			output.write("Staff ID     : "+code);
-		 			output.write(code);
-		 			output.write("\n");
-		 			output.write("First Name   :"+fname);
-		 			output.write("\n");
-		 			output.write("Middle Name  :"+mname);
-		 			output.write("\n");
-		 			output.write("Last Name    :"+lname);
-		 			output.write("\n");
-		 			output.write("Username     :"+usrname);
-		 			output.write("\n");
-		 			output.write("Password     :"+password);
-		 			System.out.println("\n\n");
-		 			System.out.println("Data Appended Successfully");	
-		 			output.close(); 
-		 			} catch (IOException e) {
-		 				System.out.println("File Append Error");	
-		 			} 
-	 
-		    	 break; 
-		    	 
-		    		    	 
-		    	 case 'd': 
+		    		 //OPEN FILE staff.txt
+		    		 try
+		    		 {
+		    			 output = new Formatter ("staff.txt"); //open the file
+		    		 }
+		    		 catch (SecurityException securityException)
+		    		 {
+		    			 System.err.println("Write permission denied. Terminating.");
+		    			 System.exit(1); // terminate the program
+		    		 }
+		    		 catch (FileNotFoundException fileNotFoundException)
+		    		 {
+		    			 System.err.println("Error opening file. Terminating.");
+		    			 System.exit(1); // terminate the program
+		    		 }
+		    		 
+		    		 //ADD RECORDS TO FILE
+		    		 System.out.printf("%s%n%s%n", "Enter staff ID, first name, middle name, last name, username and password.", "Enter end-of-file indicator (Ctrl+Z) to end input.");
+		    		 
+		    		 while (input.hasNext()) // loop until end-of-file indicator
+		    		 {
+		    			 try
+		    			 {
+		    				 // output new record to file; assumes valid input
+							 output.format("%d %s %s %s %s %s%n", input.nextInt(), input.next(), input.next(), input.next(), input.next(), input.next());
+						 }
+						 catch (FormatterClosedException formatterClosedException)
+						 {
+							 System.err.println("Error writing to file. Terminating.");
+							 break;
+						 }
+						 catch (NoSuchElementException elementException)
+						 {
+							 System.err.println("Invalid input. Please try again.");
+							 input.nextLine(); // discard input so user can try again
+						 }
+							
+						 //System.out.print("? ");
+					 }// end while
+								
+		    	 	 // close file
+					 if (output != null)
+						 output.close();
+					
+					 
+					 //OPEN FILE staff.txt
+					 try
+					 {
+						 input = new Scanner(Paths.get("staff.txt"));
+					 }
+					 catch (IOException ioException)
+					 {
+						 System.err.println("eRROR OPENING FILE. Terminsting.");
+						 System.exit(1);
+					 }
+					 
+					 //READ RECORD FROM FILE
+					 System.out.println("\n\n");
+					 System.out.printf("%-12s%-12s%-12s%-12s%10s%10s%n", "ID Number", "First Name", "Middle Name", "Last Name", "Username", "Password");
+					 
+					 try
+					 {
+						 while (input.hasNext()) // while there is more to read
+						 {
+							 //display record contents
+							 System.out.printf("%-12d%-12s%-12s%-12s%10s%10s%n", input.nextInt(), input.next(), input.next(), input.next(), input.next(), input.next());
+						 }
+					 }
+					 catch (NoSuchElementException elementException)
+					 {
+						 System.err.println("File improperly formed. Terminating.");
+					 }
+					 catch (IllegalStateException stateException)
+					 {
+						 System.err.println("Error reading from file. Terminating. ");
+					 }
+							
+					 // close file
+					 if (input != null)
+						 input.close();		
+							
+					 break;
+							
+		    	/* case 'd': 
 		    		 
 		    		 char[] data= new char [100];
 						
@@ -195,25 +235,21 @@ public class Administrator {
 		 				System.out.println("File Read Error");
 		 			}
 		 			
-		    	 break; 
+		    	 break; */
 		    	 		  	 
 		    	 case 'q': break; 
 		    	 default :  System.out.print("\n\tInvald Input\n");
-		    	 }
-		    	 
-		     }//End while				
-				
-				
+		    	 }    	 
+		     }//End while									
 	}
 	
-	
-	// Tostring method to display attribute values
+	// toString method to display attribute values
 	public String toString()
 	{
 		String output;
 		
 		output  = "I.D                                            : "+id;
-		output +="\nFirst Name                                    : "+name.GetFname();
+		output +="\nFirst Name                                    : "+name.GetFName();
 		output +="\nMiddle Name                                   : "+name.GetMName();
 		output +="\nLast Name                                     : "+name.GetLName();
 		output +="\nUsername                                      : "+username;
@@ -221,6 +257,4 @@ public class Administrator {
 		
 		return output;
 	}
-
-
 }
