409216d383d712f47b9ae5eeea1be191eed4d679
aaliade <102116047+aaliade@users.noreply.github.com>
2022-11-25 23:01:29 -0500

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,338 +1,277 @@
-//Created By: Aaliyah Adebukunola, Aaron Brown, Kevaughn Miles, Zachary Stewart
-//ID: 2101823
-//CIT 2004 UM1 Tuesday 8am Lab Group Project
+//Authors: Aaliyah Adebukunola & Zachary Stewart
 
-package HomeAway;
+package records;
 
-
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 //Importing scanner package to receive input from the user
 import java.util.Scanner;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
-//Importing Java input/output packages to create files
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 
-
-public class Driver {	
-	
-	
-	private static JLabel label1;
-	private static JLabel label2;
-	private static JLabel label3;
-	private static JLabel label4;
-	private static JLabel label5;
-	private static JTextField selectLabel;
-	private static JButton button;
-
-
-	public static void main(String[] args) {	
+public class Driver 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("*******************************************************************\n");		
+		System.out.println("         T H E   H O M E   &   A W A Y   I N S T I T U T E         \n");		
+		System.out.println("*******************************************************************\n"); 		
 		
-		System.out.println("\t\t\t\t\t\t*******************************************************************\n");
+		String username, password;
+		String choice = "";
 		
-		System.out.println("\t\t\t\t\t\t            T H E   H O M E   &    A W A Y   I N S T I T U T E\n");
+		//Creating objects of the Scanner class
+		Scanner s = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		
-		System.out.println("\t\t\t\t\t\t*******************************************************************\n"); 
-		
-		
-		
-		while(true) {
+		Administrator adm = new Administrator(); //creating object of Administrator class
+		Staff stf = new Staff(); //creating object of Staff class
+		Student stu = new Student(); //creating object of Student class					
+		  		
+		//System Login
+		do
+		{
+			System.out.println("********************** S Y S T E M  L O G I N *********************\n");	
+			System.out.println("1: Admininstrator\n2: Staff Member\n3: Student\n0: Quit");
+			System.out.print("Enter the number corresponding with the type of user you are: ");
 			
-			
-			
-			
-			 //Creating objects of each class to manipulate data and access files
-			
-			Verification vfy = new Verification(); 
-			
-			/*
-			JPanel panel = new JPanel();
-			JFrame frame = new JFrame();
-			
-			frame.setSize(500, 400);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setVisible(true);
-			frame.add(panel);
-			
-			
-			
-			panel.setLayout(null);
-			
-			
-			
-			label1 = new JLabel("(1) Administration");
-			label1.setBounds(10,20,200,25);
-			panel.add(label1);
-			
-			label2 = new JLabel("(2) Staff");
-			label2.setBounds(10,50,200,25);
-			panel.add(label2);
-			
-			label3 = new JLabel("(3) Student");
-			label3.setBounds(10,80,200,25);
-			panel.add(label3);
-			
-			label4 = new JLabel("(4) Quit");
-			label4.setBounds(10,110,200,25);
-			panel.add(label4);
-			
-			label4 = new JLabel("Enter Selection");
-			label4.setBounds(10,140,200,25);
-			panel.add(label4);
-			
-			selectLabel = new JTextField(5);
-			selectLabel.setBounds(100, 140, 50, 25);
-			selectLabel.addActionListener(new Driver());
-			panel.add(selectLabel);
-			
-			button = new JButton("Select");
-			button.setBounds(10, 170, 100, 25);
-			panel.add(button);
-			
+			try
+			{
+				Scanner s1 = new Scanner(System.in);
+				int type = s1.nextInt();
 					
-			*/
-			
-						
-			
-			 System.out.print("\n-----------Main Menu-------------");	
-		   	 System.out.print("\n|  (1)Administrator          |");
-		   	 System.out.print("\n|  (2)Staff                  |");
-		   	 System.out.print("\n|  (3)Student                |");
-		   	 System.out.print("\n|  (4)Quit                   |");
-		   	 System.out.print("\n---------------------------------\n");
-	    	 System.out.print("Please Enter your Choice: \n");
-	    	
-	    	 try {
-	    		 
-	    		 //Creating scanner to store user input
-		    	 Scanner ch = new Scanner(System.in);
-		    	 int choice = ch.nextInt();
-		    	 
-		    	 
-		    	//Administrator validation
-		       	 if(choice==1) {
-		       		 
-		       		
-		       		
-		       		 try {
-		       			 
-		       			 String username, password;
-		       			
-				 		 Scanner s = new Scanner(System.in);
-		    			
-		    						
-		    			System.out.println("\n\n\n********** A D M I N    L O G I N **********\n");
-		    			
-		    			while (true) 
+				switch(type)
+				{
+					//Administrator
+					case 1:					
+		            	do
 		    			{
+		            		System.out.print("\nEnter username: ");
+					        username = s.nextLine();
+					        System.out.print("Enter password: ");
+					        password = s.nextLine();
+					        
+		    				String un = "admin";
+		    				String pw = "admin";
 		    				
+		    				if (username.equals(un)&& password.equals(pw))
+		    				{
+		    					break;				
+		    				}
 		    				
+		    				System.out.println("Authentication Failed. Please try again.\n");   
 		    				
-		    				System.out.print("Enter username: ");
-			 	            username = s.nextLine();
-			 	            System.out.print("Enter password: ");
-			 	            password = s.nextLine();
-			 	            vfy.adminVerify(username, password);
-			 	            break;
-			 	           
-			 	           
-		    				/*
-		    				String usrnme = JOptionPane.showInputDialog("Enter Username: ");
-		    				username = usrnme;
-		    				String psswrd = JOptionPane.showInputDialog("Enter Password: ");
-		    				password = psswrd;
-		    	            vfy.adminVerify(username, password);
-		    	            break;*/
-		    	            
-		    	            
-		    			}//End while
-		       		 
-		       			 
-		       		 }catch(Exception ex) {
-		       			 
-		       			JOptionPane.showMessageDialog(null, "Please enter valid inputs","System Error", JOptionPane.INFORMATION_MESSAGE);		 
-		       		 }
-		       		
-		       	 }//End choice 1 if
-		       	 
-		    	 
-		    	 
-				
-		    	 
-		    	 
-		    	 //Staff validation
-		    	 if(choice==2) {
-		    		 
-		    		 String username, password;
-			 			
-			 			Scanner s = new Scanner(System.in);
-			 			
-			 			System.out.println("\n\n\n*********S T A F F   L O G I N **********\n");
-			 			
-			 			while (true) 
-			 			{
-			 				System.out.print("Enter username: ");
-			 	            username = s.nextLine();
-			 	            System.out.print("Enter password: ");
-			 	            password = s.nextLine();
-			 	            vfy.staffVerify(username, password);
-			 	            break;
-			 	            
-			 	            
-			 			}//End while
-			    		 
-		    		 
-		    	 }//End choice 2 if
-		    	 
-		    	 
-		    	//Student validation
-		    	 if(choice==3) {
-		    		 
-		    		 String username, password;
-			 			
-			 			Scanner s = new Scanner(System.in);
-			 			
-			 			System.out.println("\n\n\n*********S T U D E N T   L O G I N **********\n");
-			 			
-			 			while (true) 
-			 			{
-			 				System.out.print("Enter username: ");
-			 	            username = s.nextLine();
-			 	            System.out.print("Enter password: ");
-			 	            password = s.nextLine();
-			 	            vfy.stuVerify(username, password);
-			 	            break;
-			 	            
-			 	            
-			 			}//End while
-			    		 
-		    		 
-		    	 }//End choice 3 if
-		    	 
-		    	 
-		    	 if(choice==4) {
-		    		 
-		    		 System.out.println("\n\nGoodbye!");
-		    		 System.exit(1);
-		    	 }//End choice 4 if
-		    	 
-		    	 
-		    	 if(choice!=1 && choice!=2 && choice!=3 && choice!=4) {
-		    		 
-		    		 System.out.println("\nInvalid Entry\n\n");
-		    		 continue;
-		    		 
-		    	 }//End invalid if
-		    	 
-	    		 
-	    	 }catch(Exception e) {
-	    		 
-	    		  System.err.println("Invalid Entry!");
-	    		 System.err.println("Must enter number value");
-	    			    		 
-	    	 }finally{
-	    		 
-	    		 System.out.println("\n\n");
-	    		 continue;	    		
-	    	 }
+		    			} while(true);
+		    			
+		    			System.out.println("Authentication Successful!\n");
+		    			   				    			
+		    			adm.newStaff();	//Calling newStaff method 	    			
+		    			
+		    			break;
+		    			
+		            //Staff Member
+					case 2:
+						try 
+						{
+							do
+			    			{						
+								System.out.print("\nEnter username: ");
+						        username = s.nextLine();
+						        System.out.print("Enter password: ");
+						        password = s.nextLine();
+			    		
+						        boolean valid = false;
+								String un;
+								String pw;
+								Scanner fileInp = new Scanner(new File("staffusrpass.txt"));
+									
+								while (fileInp.hasNext())
+								{
+									un = fileInp.next();
+									pw = fileInp.next();
+										
+									if(username.equals(un)&& password.equals(pw))
+									{
+										valid = true;
+										break;
+									}								
+								}
+									 
+								if (valid==true)
+								{
+									break;
+								}							
+									
+								System.out.println("Authentication Failed. Please try again.\n"); 
+																											
+			    			} while(true);
+						} 
+						catch(FileNotFoundException e)
+						{
+							System.out.println("Error: User information could not be retrieved.\n\n");						
+							break;
+						}
+		    			
+		    			System.out.println("Authentication Successful!\n\n\n");
+		    			
+						do
+						{						
+							System.out.println("------------------------ S T A F F  M E N U ------------------------");
+							System.out.println("|  (R)egister Student                                              |");
+							System.out.println("|  (C)reate Programme                                              |");
+							System.out.println("|  (D)isplay Programmes                                            |");
+							System.out.println("|  (M)odify Programme Details                                      |");
+							System.out.println("|  (G)enerate Student List                                         |");
+							System.out.println("|  (Q)uit                                                          |");
+							System.out.println("--------------------------------------------------------------------\n");
+							System.out.print("Please Enter your Choice: ");	
+							choice = input.nextLine().toLowerCase(); //converts user input to lower case
+									    	 
+							switch(choice.charAt(0))
+							{
+								//Register Student
+								case 'r':
+									stf.registerStudent();
+									break;
+								//Create Programme
+								case 'c':
+									stf.createProgramme();
+									break;
+								//Display Programmes
+								case 'd':
+									stf.displayProgrammes();
+									break;
+								//Modify Programme Details	    		
+								case 'm':
+									stf.modifyProgramme();
+									break;
+								//Generate Student List	
+								case 'g':
+									stf.generateList();
+									break;				    	
+								//Quit	    	 		  	 
+								case 'q':
+									System.out.print("\n\n\n\n");
+									break;
+								//Other	    	 
+								default:
+									System.out.print("\nInvalid Input\n\n\n");	
+									break;
+							}								    	 
+						}while (choice.charAt(0)!='q');																	
+						break;
+						
+					//Student					
+					case 3:
+						try 
+						{
+							do
+							{						
+								System.out.print("\nEnter username: ");
+						        username = s.nextLine();
+						        System.out.print("Enter password: ");
+						        password = s.nextLine();
+			    		
+						        boolean valid = false;
+								String un;
+								String pw;
+								Scanner fileInp = new Scanner(new File("stuusrpass.txt"));
+									
+								while (fileInp.hasNext())
+								{
+									un = fileInp.next();
+									pw = fileInp.next();
+										
+									if(username.equals(un)&& password.equals(pw))
+									{
+										valid = true;
+										break;
+									}								
+								}
+									 
+								if (valid==true)
+								{
+									break;
+								}							
+									
+								System.out.println("Authentication Failed. Please try again.\n"); 																										
+							} while(true);
+						} 
+						catch(FileNotFoundException e)
+						{
+							System.out.println("Error: User information could not be retrieved.\n\n");						
+							break;
+						}
+		    			
+		    			System.out.println("Authentication Successful!\n\n\n");  
 			
+						do
+						{			 
+							System.out.println("---------------------- S T U D E N T  M E N U ----------------------");
+							System.out.println("|  (V)iew Programme Details                                        |");
+							System.out.println("|  (A)dd Course                                                    |");
+							System.out.println("|  (F)Fee Breakdown                                                |");
+							System.out.println("|  (P)Progress Report                                              |");
+							System.out.println("|  (Q)uit                                                          |");
+							System.out.println("--------------------------------------------------------------------\n");
+							System.out.print("Please Enter your Choice: ");								    	 
+							choice = input.nextLine().toLowerCase(); 
+									    	 
+							switch(choice.charAt(0))
+							{
+								//View Programme Details
+								case 'v':
+									stu.viewProgram();
+									break;
+								//Add Course
+								case 'a':
+									stu.addCourse();
+									break;
+								//Generate Fee Breakdown	    		
+								case 'f':
+									stu.generateFee();
+									break;
+								//Generate Progress Report	
+								case 'p':
+									stu.generateProgressReport();
+									break;				    	
+								//Quit	    	 		  	 
+								case 'q':
+									System.out.print("\n\n\n\n");	
+									break;
+								//Other	    	 
+								default:
+									System.out.print("\nInvalid Input\n\n\n");	
+									break;
+							}								    	 
+						}while(choice.charAt(0)!='q');																	
+						break;
+						
+					//Quit
+					case 0:
+						System.exit(1);
+						break;
+						
+						
+					//Other	
+					default:  
+						System.out.println("\nInvalid Input\n");					
+						break;
+				}	
+			}
+			catch(InputMismatchException ime) 
+			{
+				System.out.println("\nInvalid Entry! Must enter number value.\n");
+	    		continue;		    		 
+	    	}
+			catch(NoSuchElementException nsee) 
+			{
+				System.out.println("\n\n");
+	    		continue;		    		 
+	    	}
 			
-		}//End outer while	
+		}while(true);	
 		
-        
-        
 	}//End main
-
-	/*
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		int choice = Integer.parseInt(selectLabel.getText());
-		
-		 Verification vfy = new Verification();
-				
-		//Administrator validation
-   	 if(choice==1) {
-   		 
-   		
-   		
-   		 try {
-   			 
-   			 String username, password;
-			
-						
-			System.out.println("\n\n\n********** A D M I N    L O G I N **********\n");
-			
-			while (true) 
-			{
-				String usrnme = JOptionPane.showInputDialog("Enter Username: ");
-				username = usrnme;
-				String psswrd = JOptionPane.showInputDialog("Enter Password: ");
-				password = psswrd;
-	            vfy.adminVerify(username, password);
-	            break;
-	            
-	            
-			}//End while
-   		 
-   			 
-   		 }catch(Exception ex) {
-   			 
-   			JOptionPane.showMessageDialog(null, "Please enter valid inputs","System Error", JOptionPane.INFORMATION_MESSAGE);		 
-   		 }
-   		
-   	 }//End choice 1 if
-   	 
-   	 
-   	 	
- 	//Administrator validation
-   	 if(choice==2) {
-   		 
-   		
-   		
-   		 try {
-   			 
-   			 String username, password;
-						
-						
-			while (true) 
-			{
-				String usrnme = JOptionPane.showInputDialog("Enter Username: ");
-				username = usrnme;
-				String psswrd = JOptionPane.showInputDialog("Enter Password: ");
-				password = psswrd;
-	            vfy.adminVerify(username, password);
-	            break;
-	            
-	            
-			}//End while
-   		 
-   			 
-   		 }catch(Exception ex) {
-   			 
-   			JOptionPane.showMessageDialog(null, "Sysyem Error!");		 
-   		 }
-   		
-   	 }//End choice 2 if
-		
-	}*/
 	
-
-	
-
 }//End class
